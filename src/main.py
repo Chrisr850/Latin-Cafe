1c351496fc284b2b448c83d1dbdcafecee31c14a
Dant86 <vedantdpathak@iCloud.com>
2017-10-21 23:08:38 -0500

    alright made this more generic but this might lead the sitr to some breakage ... well see

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/<string:page_name>')
+def hello_world(page_name):
+    return render_template("{}.html".format(page_name))
