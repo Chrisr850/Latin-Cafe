484dcb35df1e71c4a269541e417d4b89f51ef0d2
Dant86 <vedantdpathak@iCloud.com>
2018-01-12 11:30:58 -0600

    website only needs to be static boi

--- src/main.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/<string:page_name>')
-def hello_world(page_name):
-    return render_template("{}.html".format(page_name))
