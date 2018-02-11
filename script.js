69d9256298eb2a364c7fa40aff34c3f50d3b8dd3
Dant86 <vedantdpathak@iCloud.com>
2018-02-11 15:13:23 -0600

    made cosmetic changes

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var menu = {
-	"coffee": 2.5,
-	"tea": 2.5,
-	"hot chocolate": 2
+	"coffee": 2.50,
+	"tea": 2.50,
+	"hot chocolate": 2.00
 };
+
+var total_price = 0.00
 
 function load_divs() {
 	$.each(menu, function(key, value) {
-		$('#lhs').append('<div class=\'menuitem\' id=\'' + key + '\'>' + key + ': ' + value + '</div>');
+		$('#lhs').append('<div class=\'menuitem\' id=\'' + key + '\'>' + key + ': $' + value.toFixed(2) + '</div>');
 	})
 }
 
 $(document).ready(function(){
 	load_divs();
 });
-
-
 
 // note that this function is temporary.
 // Instead of text, the JSON doc will include
