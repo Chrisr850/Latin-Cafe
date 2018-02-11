2356d2c66a70f031a89368ddb99f37aa93491f0d
Dant86 <vedantdpathak@iCloud.com>
2018-02-11 15:06:32 -0600

    iterates over full menu

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+var menu = {
+	"coffee": 2.5,
+	"tea": 2.5,
+	"hot chocolate": 2
+};
+
+function load_divs() {
+	$.each(menu, function(key, value) {
+		$('#lhs').append('<div class=\'menuitem\' id=\'' + key + '\'>' + key + ': ' + value + '</div>');
+	})
+}
+
+$(document).ready(function(){
+	load_divs();
+});
+
+
+
 // note that this function is temporary.
 // Instead of text, the JSON doc will include
 // The contents of the order
-$('#orderButton').on('click', function(){
+$('#orderButton').on('click', function() {
 	var temp_var = $(this).text(); // temporary
 	$.ajax({
 		type: "post",
