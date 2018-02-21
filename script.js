7a368617a84f86c4dec8b80427d19ba7f4f05664
Dant86 <vedantdpathak@iCloud.com>
2018-02-21 07:54:32 -0600

    asdf

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ var menu = {
 	"tea": 2.50,
 	"hot chocolate": 2.00
 };
-
 var order = {};
-
 var total_price = 0.00;
+var button_begin = '<button id=\'down\' id=\'';
+var div_begin = '<div id=\'btn\' class=\'btndown\'></div>';
 
-// adding menu items to website at load
-// beware, there is an extremely long line of code below
 $(document).ready(function(){
 
+	// adding menu items to website at load
+	// beware, there is an extremely long line of code below
 	$.each(menu, function(key, value) {
-		$('#lhs').append('<button id=\'down\' id=\'' + key + '\'>' + key + ': $' + value.toFixed(2) + '<div id=\'btn\' class=\'btndown\'></div>' + '</button>');
+		$('#lhs').append(button_begin+key+'\'>'+key+': $'+value.toFixed(2)+div_begin+'</button><div id=\'orderContainer\' class=\'hidden\'>Amt: <textarea rows=\'1\' cols=\'5\'></textarea><div id=\'addButton\'>Add to order</div></div>');
 	});
 
 	// init animations when click on menu item
@@ -22,17 +22,18 @@ $(document).ready(function(){
 		if($(this).attr("id") === "down") {
 			$(this).removeAttr("id");
 			$(this).attr("id", "up");
-			console.log("up");
-			var btn = $(this).find("div");
+			var btn = $(this).find("#btn");
 			btn.removeClass("btndown");
 			btn.addClass("btnup");
+			$(this).find("#orderContainer").removeClass("hidden");
 		}
 		else {
 			$(this).removeAttr("id");
 			$(this).attr("id", "down");
-			var btn = $(this).find("div");
+			var btn = $(this).find("#btn");
 			btn.removeClass("btnup");
 			btn.addClass("btndown");
+			$(this).find("#orderContainer").addClass("hidden");
 		}
 	});
 
@@ -42,13 +43,12 @@ $(document).ready(function(){
 		The contents of the order
 	*/
 	$('#orderButton').on("click", function(){
-		console.log("this is function");
 		var temp_var = $(this).text(); // temporary
 		$.ajax({
 			type: "post",
 			dataType: "json",
 			url: "http://10.74.1.3:5000/",
-			crossDoman: true,
+			crossDomain: true,
 			contentType: "application/json",
 			data: JSON.stringify({'text': temp_var}),
 			processData: false
